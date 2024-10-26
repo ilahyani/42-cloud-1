ad5a11e0fcb389c9cc41ed5e7951e83611903a17
ilahyani <lahyani799@gmail.com>
2024-10-26 10:43:52 +0100

    inception setup

---
+++ inception/services/mariadb/tools/run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+sed -i 's/127.0.0.1/0.0.0.0/g' etc/mysql/mariadb.conf.d/50-server.cnf
+
+service mysql start
+
+echo "CREATE DATABASE IF NOT EXISTS ${DATABASE};" | mysql
+echo "GRANT ALL PRIVILEGES ON ${DATABASE}.* TO '${USER}'@'%' IDENTIFIED BY '${USER_PASSWD}';" | mysql
+echo "GRANT ALL PRIVILEGES ON *.* TO 'root'@localhost IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';" | mysql
+
+echo ${MYSQL_ROOT_PASSWORD}
+
+mysqladmin shutdown -uroot -p${MYSQL_ROOT_PASSWORD}
+
+mysqld_safe
