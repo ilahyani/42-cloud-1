7ac3d9a33e9ef000103f423477b6650531c63b97
Smail Lahyani <ilahyani@student.1337.ma>
2024-11-01 10:39:59 +0100

    removing echo command

--- inception/services/mariadb/tools/run.sh
+++ inception/services/mariadb/tools/run.sh
@@ -8,8 +8,6 @@ echo "CREATE DATABASE IF NOT EXISTS ${DATABASE};" | mysql
 echo "GRANT ALL PRIVILEGES ON ${DATABASE}.* TO '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';" | mysql
 echo "GRANT ALL PRIVILEGES ON *.* TO 'root'@localhost IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';" | mysql
 
-echo ${MYSQL_ROOT_PASSWORD}
-
 mysqladmin shutdown -uroot -p${MYSQL_ROOT_PASSWORD}
 
 mysqld_safe
