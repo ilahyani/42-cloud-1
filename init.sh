1320a43840870375782a15b4baf0bb4ee7987d0b
Smail Lahyani <ilahyani@student.1337.ma>
2024-11-09 18:52:56 +0100

    [refactor] clean up and remove comments

--- init.sh
+++ init.sh
@@ -29,16 +29,12 @@ terraform apply --auto-approve -var="aws_access_key=$AWS_ACCESS_KEY_ID" -var="aw
 INSTANCE_IP=$(terraform output -raw public_ip)
 instanceid=$(terraform output -raw instance_id)
 echo -e "" | tee  -a /cloud/inception/.env
-# echo -e "INSTANCE_IP=$INSTANCE_IP" | tee  -a /cloud/inception/.env
-# echo -e "instanceid=$instanceid" | tee  -a /cloud/inception/.env
 echo -e "AWS_REGION=$AWS_REGION" | tee  -a /cloud/inception/.env
 echo -e "GF_ACCESS_KEY_ID=$(terraform output -raw access_key_id)" | tee  -a /cloud/inception/.env
 echo -e "GF_SECRET_ACCESS_KEY=$(terraform output -raw secret_access_key)" | tee  -a /cloud/inception/.env
 
 cd /cloud
 
-    # instanceid=$(cat inception/.env | grep instanceid | cut -d '=' -f 2)
-    # INSTANCE_IP=$(cat inception/.env | grep INSTANCE_IP | cut -d '=' -f 2)
 scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i infrastructure/aws_ec2_key.pem inception/.env ubuntu@"${INSTANCE_IP}":inception/.env
 ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i infrastructure/aws_ec2_key.pem ubuntu@"${INSTANCE_IP}" << EOF
     sed -i "s/\\\$instanceid/${instanceid}/g" inception/services/grafana/dashboards/files/ec2.json
